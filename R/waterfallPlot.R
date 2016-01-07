576a3dc37b8595cdfadd941a4a017eed25f0232b
Jess Chandler <jchandler@emiconsulting.com>
2016-01-07 10:38:46 +0900

    minor update to plot
    modify palette to only use two colors
    fix lines to allow a decrease to follow an increase

--- R/waterfallPlot.R
+++ R/waterfallPlot.R
@@ -3,10 +3,10 @@
 #'waterfallPlot() : a function that takes a dataframe and creates a waterfall plot as a ggplot2 object which can be further modified
 #'
 #' @param df a dataframe with columns 'variable' (character), 'total' (numeric),  'base' (numeric), 'increase' (numeric), and 'decrease' (numeric). can have additional columns, ignored. Assumed to have starting value as first row and ending value as last row.
-#' @param palette, default is c("#d7191c","#fdae61","#abdda4","#2b83ba")
+#' @param palette, default is c("#d7191c","#2b83ba")
 #' @param xlab title the x axis, default is ""
 #' @param ylab title the y axis, default is ""
-#' @param xfactors label the x axis, default is df$variable
+#' @param xfactors label the x axis; must equal the number of variables, default is df$variable
 #' @param offset which sets the width of the floating segments, default is "0.3"
 #' @import dplyr ggplot2 scales
 #' @export
@@ -26,14 +26,14 @@
 #' # without intermediate tables, print the gross.permute plot
 #' rawparamdf <- data.frame( # lighting example
 #'                          params = c("ISR","deltaWatts","HOU","x"),
-#'                          value = c(0.5, 0.7, 1.2, 1.5),
+#'                          value = c(.5, 1.7, .2, 1.5),
 #'                          stringsAsFactors = FALSE
 #'                         )
 #' waterfallPlot(waterfallPrep(rawparamdf, 200, .6, .8, output="gross"))
 #'
 #' @return a ggplot2 object
 waterfallPlot <- function(df,
-                          palette=c("#d7191c","#fdae61","#abdda4","#2b83ba"),
+                          palette=c("#d7191c","#2b83ba"),
                           xlab="" ,ylab="", xfactors=NULL, offset=0.3) {
   if (!requireNamespace("ggplot2", quietly = TRUE)) {
     stop("ggplot2 needed for this function to work. Please install it.",
@@ -50,7 +50,6 @@ waterfallPlot <- function(df,
   options(scipen=999) # avoid scientific notation
   ## Add the order column to the raw data frame and order appropriately
   df <- cbind(df, order = as.numeric(rownames(df)))
-  class(df)
   df <- transform(df, offset=offset)
   # create lines
   df <- df %>%
@@ -60,6 +59,8 @@ waterfallPlot <- function(df,
   for(i in 2:nrow(df)){
     if(df$max[i]==-Inf){
       df$max[i]=df$max[i-1]
+     } else if(df$max[i]==df$max[i+1]){
+        df$max[i]=df$max[i+1]
       } else {df$max[i]=df$min[i+1]}
   }
   ## Create the lines data frame to link the bars
@@ -72,14 +73,16 @@ waterfallPlot <- function(df,
                                     yend=head(liv, -1)))
   ## update the xfactors
     if(!is.null(xfactors)){
+      if(length(xfactors)==nrow(df)){
       myxlabels = xfactors
+      } else {myxlabels=df$variable} #ignore if not the right length.
     } else {myxlabels=df$variable}
   ## create the labelfill categories
   df$labelfill <- ifelse(is.na(df$decrease), "Total",
                          ifelse(df$decrease==0, "Increase",
                          ifelse(df$decrease>=0, "Decrease",
                                 NA)))
-  colnoplot <- length(unique(df$labelfill))-1
+  colnoplot <- length(unique(df$labelfill))-1 # right now this is always 2
   myPalette = palette # there is a default or provided by user
   thisPalette <- c(colorRampPalette(myPalette)(colnoplot), "dark grey")
   maxvalue <- max(c(as.numeric(df$total),
